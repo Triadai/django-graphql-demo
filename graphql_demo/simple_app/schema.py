54441a8d0f37a93f63df88198b49f7afa34ed931
Martin Brochhaus <mbrochh@gmail.com>
2017-06-07 15:30:47 +0800

    Added django-filter and a filter on the `message` field

--- graphql_demo/simple_app/schema.py
+++ graphql_demo/simple_app/schema.py
@@ -1,15 +1,18 @@
 import graphene
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter.fields import DjangoFilterConnectionField
 from . import models
 
 
 class MessageType(DjangoObjectType):
     class Meta:
         model = models.Message
+        filter_fields = {'message': ['icontains']}
+        interfaces = (graphene.Node, )
 
 
 class Query(graphene.AbstractType):
-    all_messages = graphene.List(MessageType)
+    all_messages = DjangoFilterConnectionField(MessageType)
 
     def resolve_all_messages(self, args, context, info):
         return models.Message.objects.all()
